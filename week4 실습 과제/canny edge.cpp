0eeea519d41e08a676309dd7b7b11d531a5a9b60
강민영 <164704813+minykang@users.noreply.github.com>
2024-05-08 23:24:10 +0900

    Add files via upload

---
+++ week4 실습 과제/canny edge.cpp
@@ -0,0 +1,217 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <time.h>
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#define USE_OPENCV false;
+
+
+using namespace cv;
+using namespace std;
+
+clock_t start, finish;
+
+
+void followEdges(int x, int y, Mat& magnitude, int tUpper, int tLower, Mat& edges) {
+	edges.at<float>(y, x) = 255;
+
+	// 이웃 픽셀 에지 따는 과정
+	for (int i = -1; i <= 1; i++) {
+		for (int j = -1; j <= 1; j++) {
+			if ((i == 0) && (j == 0) && (x + i >= 0) && (x + j >= 0) &&
+				(x + i < magnitude.cols) && (y + j < magnitude.rows)) {
+				if ((magnitude.at<float>(y + j, x + i) > tLower) &&
+					(edges.at<float>(y + j, x + i) != 255)) {
+					followEdges(x + i, y + j, magnitude, tUpper, tLower, edges);
+					// 재귀적 방법으로 이웃 픽셀에서 확실한 edge를 찾아 edge를 구성
+				}
+			}
+		}
+	}
+}
+
+// 에지 감지 함수
+void edgeDetect(Mat& magnitude, int tUpper, int tLower, Mat& edges) {
+	int rows = magnitude.rows;
+	int cols = magnitude.cols;
+
+	edges = Mat(magnitude.size(), CV_32F, 0.0);
+
+	// 픽셀 에지따기
+	for (int x = 0; x < cols; x++) {
+		for (int y = 0; y < rows; y++) {
+			if (magnitude.at<float>(y, x) > tUpper) {
+				followEdges(x, y, magnitude, tUpper, tLower, edges);
+				// edge가 확실하면 이웃 픽셀을 따라확실한 edge를 탐색
+			}
+		}
+	}
+}
+
+// 최대가 아닌 부분은 억제하는 함수
+// 최대가 아니면 없애서 thinning과정됨
+void nonMaximumSuppression(Mat& magnitudeImage, Mat& directionImage) {
+	Mat checkImage = Mat(magnitudeImage.rows, magnitudeImage.cols, CV_8U);
+	MatIterator_<float >itMag = magnitudeImage.begin<float>();
+	MatIterator_<float >itDirection = directionImage.begin<float>();
+	MatIterator_<unsigned char>itRet = checkImage.begin<unsigned char >();
+	MatIterator_<float > itEnd = magnitudeImage.end<float>();
+
+	for (; itMag != itEnd; ++itDirection, ++itRet, ++itMag) {
+		const Point pos = itRet.pos();
+		float currentDirection = atan(*itDirection) * (180 / 3.142);
+		while (currentDirection < 0) currentDirection += 180;
+		*itDirection = currentDirection;
+		if (currentDirection > 22.5 && currentDirection <= 67.5) {
+			if (pos.y > 0 && pos.x > 0 && *itMag <= magnitudeImage.at<float>(pos.y - 1, pos.x - 1)) {
+				magnitudeImage.at<float >(pos.y, pos.x) = 0;
+			}
+			if (pos.y < magnitudeImage.rows - 1 && pos.x < magnitudeImage.cols - 1 && *itMag <= magnitudeImage.at<float >(pos.y + 1, pos.x + 1)) {
+				magnitudeImage.at<float>(pos.y, pos.x) = 0;
+			}
+		}
+		else if (currentDirection > 67.5 && currentDirection <= 112.5) {
+			if (pos.y > 0 && *itMag <= magnitudeImage.at<float >(pos.y - 1, pos.x)) {
+				magnitudeImage.at<float>(pos.y, pos.x) = 0;
+			}
+			if (pos.y < magnitudeImage.rows - 1 && *itMag <= magnitudeImage.at<float >(pos.y + 1, pos.x)) {
+				magnitudeImage.at<float>(pos.y, pos.x) = 0;
+			}
+		}
+		else if (currentDirection > 112.5 && currentDirection <= 157.5) {
+			if (pos.y > 0 && pos.x < magnitudeImage.cols - 1 && *itMag <= magnitudeImage.at<float >(pos.y - 1, pos.x + 1)) {
+				magnitudeImage.at<float >(pos.y, pos.x) = 0;
+			}
+			if (pos.y < magnitudeImage.rows - 1 && pos.x>0 && *itMag <= magnitudeImage.at<float >(pos.y + 1, pos.x - 1)) {
+				magnitudeImage.at<float >(pos.y, pos.x) = 0;
+			}
+		}
+		else {
+			if (pos.x > 0 && *itMag <= magnitudeImage.at<float >(pos.y, pos.x - 1)) {
+				magnitudeImage.at<float >(pos.y, pos.x) = 0;
+			}
+			if (pos.x < magnitudeImage.cols - 1 && *itMag <= magnitudeImage.at<float >(pos.y, pos.x + 1)) {
+				magnitudeImage.at<float >(pos.y, pos.x) = 0;
+			}
+		}
+	}
+
+}
+
+
+void ex3() {
+	cout << "--- doCannyEx() ---\n" << endl;
+
+	// 이미지 로드 
+	Mat src_img = imread("rock.png", 0);
+	if (!src_img.data) printf("No image data \n");
+
+	//Mat dst_img;
+	Mat dst_img1, dst_img2, dst_img3, dst_img4, dst_img5;
+#if USE_OPENCV
+	// < Canny 에지 탐색 수행 >
+	Canny(src_img, dst_img, 180, 240);
+#else
+	// Gaussian 필터 기반 노이즈 제거 
+	Mat blur_img;
+	GaussianBlur(src_img, blur_img, Size(3, 3), 1.5);
+
+	// Sobel필터를 통한 에지 검출 
+	Mat magX = Mat(src_img.rows, src_img.cols, CV_32F);
+	Mat magY = Mat(src_img.rows, src_img.cols, CV_32F);
+	Sobel(blur_img, magX, CV_32F, 1, 0, 3);
+	Sobel(blur_img, magY, CV_32F, 0, 1, 3);
+
+	// sobel 결과로 에지 방향 계산
+	Mat sum = Mat(src_img.rows, src_img.cols, CV_64F);
+	Mat prodX = Mat(src_img.rows, src_img.cols, CV_64F);
+	Mat prodY = Mat(src_img.rows, src_img.cols, CV_64F);
+	multiply(magX, magX, prodX);
+	multiply(magY, magY, prodY);
+	sum = prodX + prodY;
+	sqrt(sum, sum);
+
+	// < 에지 크기 계산 >
+	Mat magnitude = sum.clone();
+
+
+	//magnitude.convertTo(magnitude, CV_32F);  // 데이터 타입 변환
+
+	// Non-maximum suppression을 적용하여 엣지 변환
+	Mat slopes = Mat(src_img.rows, src_img.cols, CV_32F);
+	divide(magY, magX, slopes);
+	// gradient의 방향 계산
+	nonMaximumSuppression(magnitude, slopes);
+
+	// < Edge tracking by hysteresis >
+	/*edgeDetect(magnitude, 200, 100, dst_img);*/
+	/*dst_img.convertTo(dst_img, CV_8UC1);*/
+#endif
+
+	//edge tracking by hysteresis
+	start = clock();
+	edgeDetect(magnitude, 200, 100, dst_img1);
+	finish = clock();
+	cout << (float)(finish - start) << "ms" << endl;
+	start = clock();
+	edgeDetect(magnitude, 150, 100, dst_img2);
+	finish = clock();
+	cout << (float)(finish - start) << "ms" << endl;
+	start = clock();
+	edgeDetect(magnitude, 100, 50, dst_img3);
+	finish = clock();
+	cout << (float)(finish - start) << "ms" << endl;
+	start = clock();
+	edgeDetect(magnitude, 200, 30, dst_img4);
+	finish = clock();
+	cout << (float)(finish - start) << "ms" << endl;
+	start = clock();
+	edgeDetect(magnitude, 150, 50, dst_img5);
+	finish = clock();
+	cout << (float)(finish - start) << "ms" << endl;
+
+	dst_img1.convertTo(dst_img1, CV_8UC1);
+	dst_img2.convertTo(dst_img2, CV_8UC1);
+	dst_img3.convertTo(dst_img3, CV_8UC1);
+	dst_img4.convertTo(dst_img4, CV_8UC1);
+	dst_img5.convertTo(dst_img5, CV_8UC1);
+
+
+
+
+	// 결과 이미지 생성 세로로 합침
+	Mat result_img, result_img2, result_img2_2, result_img3;
+	hconcat(src_img, dst_img1, result_img);
+	hconcat(dst_img2, dst_img3, result_img2);
+	hconcat(dst_img4, dst_img5, result_img3);
+	// 가로로 합침
+	vconcat(result_img, result_img2, result_img2_2);
+	vconcat(result_img2, result_img3, result_img3);
+
+
+	// 각 결과 이미지를 따로 표시
+	imshow("Result Image 1", result_img);
+	waitKey(0); // 사용자가 키를 누를 때까지 기다림
+
+	imshow("Result Image 2", result_img2_2);
+	waitKey(0); // 사용자가 키를 누를 때까지 기다림
+
+	imshow("Result Image 3", result_img3);
+	waitKey(0); // 사용자가 키를 누를 때까지 기다림
+
+}
+
+
+int main() {
+	
+
+	
+	ex3();
+
+
+
+	return 0;
+}
---
+++ week4 실습 과제/median filter.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <time.h>
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#define USE_OPENCV false;
+
+
+using namespace cv;
+using namespace std;
+
+clock_t start, finish;
+
+// 직접 salt_pepper noise를 없애기 위해 만든 함수 
+// 중앙값 필터링을 수행하는 함수
+void myMedian(const Mat& src_img, Mat& dst_img, const Size& kn_size) {
+	dst_img = Mat::zeros(src_img.size(), CV_8UC1);
+
+	int wd = src_img.cols; // 이미지의 너비
+	int hg = src_img.rows; // 이미지의 높이
+	int kwd = kn_size.width; // 커널 너비
+	int khg = kn_size.height; // 커널 높이
+	int rad_w = kwd / 2; // 반경 너비
+	int rad_h = khg / 2; // 반경 높이
+
+	uchar* src_data = (uchar*)src_img.data;// 입력 이미지 데이터
+	uchar* dst_data = (uchar*)dst_img.data;// 출력결과 이미지 데이터
+
+	float* table = new float[kwd * khg]; // 커널 데이터를 동적할당한다
+	float tmp;
+
+	// 커널 안에서 각각의 픽셀에 대해서 수행
+	for (int c = rad_w; c < wd - rad_w; c++) {
+		for (int r = rad_h; r < hg - rad_h; r++) {
+			tmp = 0.0f;
+			for (int kc = -rad_w; kc <= rad_w; kc++) {
+				for (int kr = -rad_h; kr <= rad_h; kr++) {
+					tmp = (float)src_data[(r + kr) * wd + (c + kc)];
+					table[(kr + rad_h) * kwd + (kc + rad_w)] = tmp;
+				}
+			}
+			sort(table, table + kwd * khg); // 커널 데이터를 정렬
+			dst_data[r * wd + c] = (uchar)table[(kwd * khg) / 2]; // 중간값을 결과 이미지에 적용
+		}
+	}
+
+	delete[] table; // 동적 할당된 메모리를 해제한다
+}
+
+//median함수를 만든것을 수행하게 하는 함수
+void ex1() {
+	cout << "--- doMedianEx() ---\n" << endl;
+
+	// 이미지 불러오기
+	Mat src_img = imread("salt_pepper.png", 0);
+	if (!src_img.data) {
+		printf("No image data \n");
+	}
+
+	// Median 필터링함수를 수행 수행
+	Mat dst_img;
+#ifdef USE_OPENCV
+	medianBlur(src_img, dst_img, 5);
+#else
+	myMedian(src_img, dst_img, Size(5, 5));
+#endif
+
+	// 결과 출력
+	Mat result_img;
+	hconcat(src_img, dst_img, result_img); // 원본과 결과를 가로로 연결한다
+	imshow("doMedianEx()", result_img);
+	waitKey(0);
+}
+
+
+
+
+
+int main() {
+	
+	ex1();
+
+
+
+	return 0;
+}
